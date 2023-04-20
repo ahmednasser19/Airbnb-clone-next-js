a6667f6d29f2ae4262f6e4f3a60fcdb5dae6daa2
ahmed nasser <ahmednas197@gmail.com>
2023-04-20 22:05:50 +0200

    Added Empty state component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,23 @@
+import ClientOnly from "./components/ClientOnly";
+import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
+
 export default function Home() {
-  return <div className="text-[42px] text-rose-500 ">Hello airbnb</div>;
+  const isEmpty = true;
+  if (isEmpty) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    );
+  }
+  return (
+    <ClientOnly>
+      <Container>
+        <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8 ">
+          <div>My Future Listing</div>
+        </div>
+      </Container>
+    </ClientOnly>
+  );
 }
